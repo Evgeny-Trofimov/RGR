c207076b727e6e9111e4c2405cacf956aaad2598
Evgeny-Trofimov <evgentrofimovevgen@yandex.ru>
2025-11-10 01:07:53 +0000

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <cstdint>
+
+extern uint64_t DH_PRIME;
+extern uint64_t DH_GENERATOR;
+extern uint64_t SHAMIR_PRIME;
+extern const uint64_t EL_GAMAL_PRIME;
+extern const uint64_t EL_GAMAL_GENERATOR;
+
+#endif
