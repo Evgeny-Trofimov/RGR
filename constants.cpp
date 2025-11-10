733cebec94f076e31379fdf3a2038b8d5373f4ce
Evgeny-Trofimov <evgentrofimovevgen@yandex.ru>
2025-11-10 01:07:29 +0000

    Create constants.cpp

---
+++ constants.cpp
@@ -0,0 +1,7 @@
+#include "constants.h"
+
+const uint64_t DH_PRIME = 4294967291;
+const uint64_t DH_GENERATOR = 5;
+const uint64_t SHAMIR_PRIME = 4294967311;
+const uint64_t EL_GAMAL_PRIME = 4294967311;
+const uint64_t EL_GAMAL_GENERATOR = 3;
